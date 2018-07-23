d71dd294d4710e4c4a4e8b56cff623c1531978b5
Gatare Libère <gatarelib@gmail.com>
2018-07-23 21:46:04 +0300

    Update WAcode.js

--- WAcode.js
+++ WAcode.js
@@ -5,7 +5,7 @@
  company_logo_url: <a href="https://icons8.com">Icon pack by Icons8</a>, // URL with company logo
  greeting_message: " Hello, the first message to be displayed, type here. ", // Main text
  call_to_action: " A message inviting you to send a whatsapp. ", // Call for action
- position: " right ", // Position of widget on page 'right' or 'left'
+ position: " left ", // Position of widget on page
  };
  var proto = document.location.protocol, host = "whatshelp.io", url = proto + "// static." + host;
  var s = document.createElement ('script'); s.type = 'text / javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
